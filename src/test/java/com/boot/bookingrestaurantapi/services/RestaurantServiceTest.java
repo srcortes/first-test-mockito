7c6a95d4b4cc9083190db97b67c5e5f7577b0719
= <=>
2020-09-25 22:07:55 -0500

    se sube test de service

--- src/test/java/com/boot/bookingrestaurantapi/services/RestaurantServiceTest.java
+++ src/test/java/com/boot/bookingrestaurantapi/services/RestaurantServiceTest.java
@@ -1,8 +1,11 @@
 package com.boot.bookingrestaurantapi.services;
 
+import static org.junit.Assert.fail;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.assertj.core.util.Arrays;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -15,6 +18,7 @@ import com.boot.bookingrestaurantapi.entities.Reservation;
 import com.boot.bookingrestaurantapi.entities.Restaurant;
 import com.boot.bookingrestaurantapi.entities.Turn;
 import com.boot.bookingrestaurantapi.exceptions.BookingException;
+import com.boot.bookingrestaurantapi.jsons.RestaurantRest;
 import com.boot.bookingrestaurantapi.jsons.TurnRest;
 import com.boot.bookingrestaurantapi.repositories.RestaurantRepository;
 import com.boot.bookingrestaurantapi.services.impl.ReservationServiceImpl;
@@ -23,27 +27,29 @@ import com.boot.bookingrestaurantapi.services.impl.RestaurantServiceImpl;
 import java.util.Optional;
 
 public class RestaurantServiceTest {
-	
+
 	private static final Long RESTAURANT_ID = 1L;
 	private static final Restaurant RESTAURANT = new Restaurant();
 	private static final List<Turn> TURN = new ArrayList<>();
 	private static final List<Board> BOARD = new ArrayList<>();
 	private static final List<Reservation> RESERVATION = new ArrayList<>();
+	private static final List<Restaurant> LIST_RESTAURANT = new ArrayList<>();
+	
 	private static final String NAME = "Burger";
 	private static final String DESCRIPTION = "Todo tipo de hamburguesas";
 	private static final String ADDRES = "Av. Galindo";
 	private static final String IMAGE = "www.image.com";
-	
+
 	@Mock
 	RestaurantRepository restaurantRepository;
-	
+
 	@InjectMocks
 	RestaurantServiceImpl restaurantServiceImpl;
-	
+
 	@Before
 	public void init() {
 		MockitoAnnotations.initMocks(this);
-		
+
 		RESTAURANT.setName(NAME);
 		RESTAURANT.setDescription(DESCRIPTION);
 		RESTAURANT.setAddress(ADDRES);
@@ -52,12 +58,24 @@ public class RestaurantServiceTest {
 		RESTAURANT.setTurns(TURN);
 		RESTAURANT.setBoards(BOARD);
 		RESTAURANT.setReservations(RESERVATION);
-		
+	}
+
+	@Test
+	public void getRestaurantByIdTest() throws BookingException {
+		Mockito.when(restaurantRepository.findById(RESTAURANT_ID)).thenReturn(Optional.of(RESTAURANT));
+		restaurantServiceImpl.getRestaurantById(RESTAURANT_ID);
+	}
+	
+	@Test(expected = BookingException.class)
+	public void getRestaurantByIdTestError() throws BookingException{
+		Mockito.when(restaurantRepository.findById(RESTAURANT_ID)).thenReturn(Optional.empty());
+		restaurantServiceImpl.getRestaurantById(RESTAURANT_ID);
+		fail();
 	}
 	
 	@Test
-	public void getRestaurantByIdTest() throws BookingException{
-		Mockito.when(restaurantRepository.findById(RESTAURANT_ID)).thenReturn(Optional.of(RESTAURANT));
-		restaurantServiceImpl.getRestaurantById(RESTAURANT_ID);
+	public void getRestaurantsTest()throws BookingException {
+		Mockito.when(restaurantRepository.findAll()).thenReturn(LIST_RESTAURANT);
+		restaurantServiceImpl.getRestaurants();
 	}
 }
